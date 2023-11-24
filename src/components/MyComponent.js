3dc5517dd26c501ed075f5508192a7a18c351a51
huyeduon <huyeduon@cisco.com>
2023-11-24 19:05:13 +0800

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,12 +7,22 @@ class MyComponent extends React.Component {
     age: 26,
   };
 
+  handleClick(event) {
+    console.log(">> click me my button");
+    console.log("My name is", this.state.name);
+  }
+
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name}
         and I'm from {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
