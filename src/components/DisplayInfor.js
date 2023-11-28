c817942432dc0cb15fb8a6574bc6bf89888da494
huyeduon <huyeduon@cisco.com>
2023-11-28 15:54:25 +0800

    video 37

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,15 +1,37 @@
-import React from "react";
-import "./DisplayInfor.scss";
-import logo from "./../logo.svg";
+import React from 'react';
+import './DisplayInfor.scss';
+import logo from './../logo.svg';
+
 class DisplayInfor extends React.Component {
-  state = {
-    isShowListUser: true,
-  };
+  constructor(props) {
+    console.log('>>> call construtor');
+    super(props);
+    this.state = {
+      isShowListUser: true,
+    };
+  }
+
+  componentDidMount() {
+    console.log('>>> call me component did mount');
+    setTimeout(() => {
+      document.title = 'Eric hoi dan IT';
+    }, 3000);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('>>> call me component update', this.props, prevProps);
+    if (this.props.listUsers !== prevProps.listUsers) {
+      if (this.props.listUsers.length === 5) {
+        alert('You got 5');
+      }
+    }
+  }
 
   handleShowHide = () => {
     this.setState({ isShowListUser: !this.state.isShowListUser });
   };
   render() {
+    console.log('>>> call me render');
     // destructuring array/object
     const { listUsers } = this.props;
     // const listUsers = this.props.listUsers
@@ -22,15 +44,15 @@ class DisplayInfor extends React.Component {
             }}
           >
             {this.state.isShowListUser === true
-              ? "Hide list users"
-              : "Show list users"}
+              ? 'Hide list users'
+              : 'Show list users'}
           </span>
         </div>
         {this.state.isShowListUser && (
           <>
-            {listUsers.map((user) => {
+            {listUsers.map(user => {
               return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
                   <div>My name's {user.name}</div>
                   <div>My age's {user.age}</div>
                   <div>
