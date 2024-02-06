41e6c411c36db9a44be1e5353b9fbfb33ef43fd6
huyeduon <huyeduon@cisco.com>
2024-02-06 22:27:19 +0800

    no new

--- src/components/Admin/Admin.js
+++
@@ -1,5 +0,0 @@
-const Admin = props => {
-  return <div>admin component</div>;
-};
-
-export default Admin;
--- src/components/User/User.js
+++
@@ -1,5 +0,0 @@
-const User = props => {
-  return <div>user component</div>;
-};
-
-export default User;
