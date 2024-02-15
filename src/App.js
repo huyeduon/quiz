efc6845dce85f8c7c609693e25a8d84fb662db95
huyeduon <huyeduon@cisco.com>
2024-02-16 00:51:22 +0800

    add non-redux counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
+
+  const [counter1, setCounter1] = useState(0);
+
+  const increment = () => {
+    setCounter1(prevCounter => prevCounter + 1);
+  };
 
   return (
     <div className="App">
@@ -16,6 +23,8 @@ const App = () => {
         <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+        Value: {counter1}
+        <button onClick={increment}>Increment</button>
       </header>
     </div>
   );
